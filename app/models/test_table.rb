78ffb88912e7105fdc76ab225deda33bb52e25ac
Britt Ballard <britt@thoughtbot.com>
2015-05-01 15:02:08 -0500

    more incremental steps

--- app/models/test_table.rb
+++ app/models/test_table.rb
@@ -1,6 +1,10 @@
 class TestTable < ActiveRecord::Base
-  validate do |test_table|
-    the_object.valid?
+  validate :column_to_object_valid?
+
+  def column_to_object_valid?
+    unless the_object.valid?
+      errors.add(:the_object, the_object.errors.full_messages)
+    end
   end
 
   def the_object
@@ -12,14 +16,20 @@ class TheObject
   include ActiveModel
   include ActiveModel::Validations
 
+  validates :hi, presence: true
+
   def initialize(attributes)
-    validations = attributes.delete("validations")
+    @json_values = attributes
 
-    attributes.each do |key, value|
-      self.class.send(:attr_accessor, key)
+    attributes.except("validations").each do |key, value|
+      self.class.send(:attr_reader, key)
       instance_variable_set("@#{key}", value)
+      define_method "#{key}=" do |arg|
+        instance_variable_set("@#{key.clone}", arg)
+        @json_values[key.clone] = arg
+      end
 
-      validations.try(:[], key).try(:each) do |validation|
+      attributes["validations"].try(:[], key).try(:each) do |validation|
         self.class.send("validates_#{validation}", key)
       end
     end
